691d2d1e122798466459577fc35089a440c28214
Vikas2593 <Vikas2593@users.noreply.github.com>
2026-08-25 22:30:05 +0530

    Updated test description

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -14,7 +14,7 @@ test.describe("Login Tests", () => {
     await expect(page.locator("h6")).toHaveText("Dashboard");
   });
 
-  test("User can login with invalid credentials", async ({ page }) => {
+  test("User can't login with invalid credentials", async ({ page }) => {
     const loginpage = new LoginPage(page);
     await loginpage.login("invalid_user", "invalid_user");
     await expect(page.getByRole('alert')).toBeVisible();
